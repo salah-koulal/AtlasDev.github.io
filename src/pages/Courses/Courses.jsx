e18b16d9ef3ba8e335bc9f4c3f31a917d151e6ab
salah-koulal <salah.rajawi289@gmail.com>
2024-02-17 18:17:20 +0100

    dlt

--- src/pages/Courses/Courses.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Courses = () => {
-    return <div>Courses</div>
-};
-
-export default Courses
-
--- src/pages/Courses/CoursesDetails.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const CoursesDetails = () => {
-    return <div>CoursesDetails</div>
-};
-
-export default CoursesDetails
-
